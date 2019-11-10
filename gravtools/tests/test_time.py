cdfae852eda915b1fa3962e32f1b0ca28bad0d47
JWKennington <Jimmy.Kennington@gmail.com>
2019-11-10 02:39:33 -0600

    Add coercion for time conversion (#4)
    * adding time conversion util
    
    * add coercion for LIGOTimeGPS
    
    * bump version

--- gravtools/tests/test_time.py
+++ gravtools/tests/test_time.py
@@ -2,6 +2,8 @@
 import datetime
 
 import pytest
+from lal import LIGOTimeGPS
+
 from gravtools import time
 
 
@@ -10,5 +12,10 @@ class TestTime:
     def gps_time(self):
         return 1187529241
 
-    def test_gps_to_datetime(self, gps_time):
+    @pytest.fixture(scope='class', autouse=True)
+    def gps_time2(self):
+        return LIGOTimeGPS(1187529241)
+
+    def test_gps_to_datetime(self, gps_time, gps_time2):
         assert time.gps_to_datetime(gps_time) == datetime.datetime(2017, 8, 23, 13, 14, 20)
+        assert time.gps_to_datetime(gps_time2) == datetime.datetime(2017, 8, 23, 13, 14, 20)
